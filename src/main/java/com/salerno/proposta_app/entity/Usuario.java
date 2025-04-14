f831dee59c1ad7c9338232c0b10ece8aa7300c2e
jcsalerno <contato.jcsalerno@gmail.com>
2025-04-14 18:55:56 -0300

    feat: Continuação para mapeamento MapStruct

--- src/main/java/com/salerno/proposta_app/entity/Usuario.java
+++ src/main/java/com/salerno/proposta_app/entity/Usuario.java
@@ -1,6 +1,18 @@
 package com.salerno.proposta_app.entity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Usuario {
 
--- src/main/java/com/salerno/proposta_app/mapper/PropostaMapper.java
+++ src/main/java/com/salerno/proposta_app/mapper/PropostaMapper.java
@@ -2,9 +2,20 @@ package com.salerno.proposta_app.mapper;
 import com.salerno.proposta_app.dto.PropostaRequestDto;
 import com.salerno.proposta_app.entity.Proposta;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface PropostaMapper {
+
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aprovada", ignore = true)
+    @Mapping(target = "integrada", ignore = true)
+    @Mapping(target = "observacao", ignore = true)
 
     Proposta convertDtoToProposta(PropostaRequestDto propostaRequestDto);
 }
