e282a3ba554821572291595c7ac491d2542e4e8b
jcsalerno <contato.jcsalerno@gmail.com>
2025-04-18 20:15:27 -0300

    feat: Criação das filas no RabbitMQ

--- src/main/java/com/salerno/proposta_app/config/RabbitMQConfiguration.java
+++ src/main/java/com/salerno/proposta_app/config/RabbitMQConfiguration.java
@@ -1,8 +1,12 @@
 package com.salerno.proposta_app.config;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfiguration {
@@ -26,4 +30,14 @@ public class RabbitMQConfiguration {
     public Queue criarFilaPropostaConcluidaMsNotificacao(){
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
     }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
 }
